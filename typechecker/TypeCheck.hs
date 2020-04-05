e5940003280b0c75839d370b6ab8b6e456783386
Justin <justin.baum@ninefx.com>
2020-04-05 03:54:21 -0400

    hooked up type checker

--- typechecker/TypeCheck.hs
+++ typechecker/TypeCheck.hs
@@ -11,12 +11,18 @@ import AbsCPP
 import JustinControl
 import qualified TypeCheckAux as TCA
 import ErrM
+import System.Environment   
 
 main = do
-  s <- readFile("./test2.cc")
-  putStrLn $ show 
-    (case TCA.checkProgram =<< (pProgram $ myLexer s) of
-      Ok _ -> True
-      _ -> False
-    )
+  args <- getArgs
+  if (length args > 0) then do
+    s <- readFile $ args !! 0
+    putStrLn $ 
+      (case TCA.checkProgram =<< (pProgram $ myLexer s) of
+        Ok _ -> "No type errors found."
+        Bad s -> s
+      )
+  else
+    putStrLn "Enter a filename after call, like:\
+      \n$ ./TypeCheck file.c"
 
